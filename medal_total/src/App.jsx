9d4e675b4497a45b4bc1a22281ea303a53b9b19f
noodlewd <michel35@naver.com>
2025-01-24 10:20:20 +0900

    프로젝트 최종

--- medal_total/src/App.jsx
+++ medal_total/src/App.jsx
@@ -4,7 +4,7 @@ import Button from "./components/Button";
 import ResultTable from "./components/ResultTable";
 
 const App = () => {
-  // 입력값 저장
+  // 입력값 저장, 초기값 빈 문자열, 0
   const [data, setData] = useState({
     country: "",
     gold: 0,
@@ -13,6 +13,8 @@ const App = () => {
   });
   // 입력값을 추가하고 테이블에 표시하도록 저장
   const [tableList, setTableList] = useState([]);
+
+  // 초기화용 객체
   const isReset = {
     country: "",
     gold: 0,
@@ -25,23 +27,22 @@ const App = () => {
     const { name, value } = event.target;
 
     if (value === "") {
-      setData({ ...data, [name]: value }); // 그대로 빈 문자열로 업데이트
+      setData({ ...data, [name]: value }); // 입력값이 비었을경우 그대로 출력
       return;
     }
 
-    // 0보다 크고 숫자일 경우
     if (parseFloat(value) >= 0 && !isNaN(value)) {
-      setData({ ...data, [name]: parseFloat(value) });
+      setData({ ...data, [name]: parseFloat(value) }); // 0보다 크거나 같고 숫자일 경우 변환 후 출력
       // 숫자가 아닐 경우
     } else if (isNaN(value)) {
-      setData({ ...data, [name]: value });
+      setData({ ...data, [name]: value }); // 문자열일 경우 그대로 출력
     } else {
-      event.target.value = 0;
+      event.target.value = 0; // 그 외의 경우는 0으로 출력
     }
   };
 
+  // 테이블에 데이터를 표시하는 함수
   const resultData = tableList.map((item) => {
-    // map이용하여 배열을 반환
     return {
       country: item.country,
       gold: item.gold,
@@ -50,47 +51,50 @@ const App = () => {
     };
   });
 
-  // 버튼 클릭을 통해서 input값이 나오는걸 확인함
+  // 버튼 클릭을 통해서 함수를 호출
   const createBtn = () => {
     if (data.country !== "") {
-      //some메서드를 활용
+      //some메서드를 활용하여 등록된 국가인지 값을 확인
       const isDuplication = tableList.some((item) => item.country === data.country);
 
       if (isDuplication) {
-        alert(`이미 등록된 국가입니다.`);
+        alert(`이미 등록된 국가입니다.`); // isDuplication자체가 true값이므로 중복일 경우 alert
       } else {
-        setTableList([...tableList, data]);
+        setTableList([...tableList, data]); // 국가를 추가하여 출력
+        setData(isReset);
       }
     } else {
-      alert(`국가를 등록해주세요.`);
+      alert(`국가를 등록해주세요.`); // 비어있을 경우 alert
     }
   };
-  // 삭제 로직
+
+  // 삭제버튼 클릭 시 함수를 호출
   const deleteBtn = (index) => {
-    const deleteList = tableList.filter((_, idx) => idx !== index);
+    const deleteList = tableList.filter((_, idx) => idx !== index); // 선택 국가를 제외한 나머지로 배열을 다시 생성
     setTableList(deleteList);
   };
 
-  // 업데이트 로직
+  // 업데이트 버튼 클릭 시 함수를 호출
   const updateBtn = () => {
-    let existCountry = false;
+    let existCountry = false; // 국가가 존재하는지 먼저 확인
 
     const updateList = tableList.map((item) => {
       if (item.country == data.country) {
+        // 업데이트 하려는 국가가 있을 경우
         existCountry = true;
 
         return {
-          ...item,
+          ...item, // 기존 국가 정보를 유지해준 후 나머지 데이터들 추가
           gold: item.gold + data.gold,
           silver: item.silver + data.silver,
           bronze: item.bronze + data.bronze,
         };
       } else {
-        return item;
+        return item; //   나머지 국가들 정보는 그대로 반환
       }
     });
     if (!existCountry) {
-      alert(`등록되지 않은 국가입니다.`);
+      alert(`등록되지 않은 국가입니다.`); // 업데이트 하려는 국가가 없을 경우 alert
     } else {
       setData(isReset);
       setTableList(updateList);
@@ -104,10 +108,10 @@ const App = () => {
         <div className="group">
           {/*데이터를 입력받는 컴포넌트,data와 InputChange 전달받는다*/}
           <InputBox data={data} InputChange={InputChange} />
-          {/*버튼 클릭 시 테이블에 데이터 전달(국가추가만 가능)*/}
+          {/*국가추가 및 업데이트를 처리해주는 버튼*/}
           <Button createBtn={createBtn} updateBtn={updateBtn} />
         </div>
-        {/*데이터를 전달받아 데이터를 표시하는 컴포넌트*/}
+        {/*데이터를 전달받아 테이블에 데이터를 표시하는 컴포넌트*/}
         <ResultTable resultData={resultData} deleteBtn={deleteBtn} />
       </div>
     </main>
--- medal_total/src/components/ResultTable.jsx
+++ medal_total/src/components/ResultTable.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const ResultTable = ({ resultData, deleteBtn }) => {
   return (
     <table className="tableDesign">
+      {/* props받은 배열을 순회하여 랜더링  */}
       {resultData.map((item, index) => (
         <tr key={index} className="trDesign">
           <td className="tdDesign">{item.country}</td>
@@ -10,6 +11,7 @@ const ResultTable = ({ resultData, deleteBtn }) => {
           <td className="tdDesign">{item.silver} 개</td>
           <td className="tdDesign">{item.bronze} 개</td>
           <td>
+            {/* 삭제 버튼 함수를 호출하고 삭제하려는 행의 index를 전달 */}
             <button onClick={() => deleteBtn(index)}>삭제</button>
           </td>
         </tr>
